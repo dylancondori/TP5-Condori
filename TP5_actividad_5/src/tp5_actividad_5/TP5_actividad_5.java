4150b8a3f1c4c2bfee4c7cbebd92b845095a5414
dylancondori <dcondoricorazon@gmail.com>
2026-05-25 11:41:01 -0300

    falta

---
+++ TP5_actividad_5/src/tp5_actividad_5/TP5_actividad_5.java
@@ -0,0 +1,18 @@
+package tp5_actividad_5;
+
+public class TP5_actividad_5 {
+
+    public static void main(String[] args) {
+        String [][]matriz= new String [3][3];
+        matriz[0][0]=;
+        matriz[0][1]=;
+        matriz[0][2]=;
+        matriz[1][0]=;
+        matriz[1][1]=;
+        matriz[1][2]=;
+        matriz[2][0]=;
+        matriz[2][1]=;
+        matriz[2][2]=;
+    }
+    
+}
